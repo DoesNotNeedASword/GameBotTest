e1f8163a228ca1ffc4447526fe9d4c3e9540383a
legen <legenadary.pigeon@gmail.com>
2024-10-28 00:29:19 +0300

    json serializing fixed

--- GameDomain/Models/Lobby.cs
+++ GameDomain/Models/Lobby.cs
@@ -1,13 +1,26 @@
-﻿namespace GameDomain.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace GameDomain.Models;
 
 public class Lobby
 {
     public long Id { get; }
     public string LobbyName { get; }
     public string? Password { get; }
-    public List<Player> Players { get; } = [];
-    public List<string> IpList { get; } = [];
-    public List<Player> Spectators { get; } = [];
+    public List<Player> Players { get; } = new();
+    public List<string> IpList { get; } = new();
+    public List<Player> Spectators { get; } = new();
+
+    [JsonConstructor]
+    public Lobby(long id, string lobbyName, string? password, List<Player> players, List<string> ipList, List<Player> spectators)
+    {
+        Id = id;
+        LobbyName = lobbyName;
+        Password = password;
+        Players = players;
+        IpList = ipList;
+        Spectators = spectators;
+    }
 
     public Lobby(long id, string ip, Player creator, string lobbyName, string? password)
     {
